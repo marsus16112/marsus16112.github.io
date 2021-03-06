16db41a618749b4da17df28b3adbff8ba67e52c1
Yondoku <79785221+marsus16112@users.noreply.github.com>
2021-03-05 20:22:26 -0900

    eqwfver

---
+++ index.js
@@ -0,0 +1,11 @@
+let funFact = document.getElementById('funfact');
+let factArray = [
+  'Better than my old webpage!',
+  'Dark mode, all the time!',
+  'Too many random things here!',
+  'This text is randomized!',
+  'We ask "When is 2.2?", but never "How is 2.2?" :(',
+  'missingno',
+  'Help!%20I\'m%20stuck in this textbar!',
+];
+funFact.innerHTML = factArray[Math.floor(Math.random() * factArray.length)];
